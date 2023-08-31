0f4c8f5cb3f3d9e31785377296079014ee073cfc
Len Lanphar <lanphar@gmail.com>
2023-08-31 09:04:01 -0400

    WiFi connect code

---
+++ circuitpython/README.md
@@ -0,0 +1,20 @@
+# CircuitPython WiFi example
+
+Copy `lib/adafruit_requests.mpy` from your CircuitPython library bundle into the
+`lib` directory on your CircuitPython device.
+
+Copy `code.py` from this directory to the top level of your CircuitPython device.
+
+Open the `settings.toml` file in that directory (or create it if it doesn't exist)
+and add the following lines, editing the values for your environment:
+
+```
+WIFI_SSID = "your_wifi_network_name"
+WIFI_PASSWORD = "your_wifi_password"
+LOGGING_HOST = "your_computer_ip_address"
+LOGGING_PORT = 5000
+```
+
+When you run the [server](../server), it will tell you what the IP address of your
+machine is, which is what you'll use for `LOGGING_HOST`.
+
--- circuitpython/code.py
+++ circuitpython/code.py
@@ -1,3 +1,5 @@
+import os
+import sys
 import ipaddress
 import wifi
 import socketpool
@@ -5,25 +7,33 @@ import board
 
 import adafruit_requests as requests
 
-# Get wifi details and host IP from a secrets.py file
-try:
-    from secrets import secrets
-except ImportError:
-    print("WiFi secrets are kept in secrets.py, please add them there!")
-    raise
+# load the configuration settings from settings.toml
+settings = {} 
+settings_keys = ["WIFI_SSID","WIFI_PASSWORD","LOGGING_HOST","LOGGING_PORT"]
+for key in settings_keys:
+    settings[key] = os.getenv(key)
+    if settings[key] is None:
+        print(f'Missing {key} setting in settings.toml')
+        sys.exit(1)
 
 # URLs to fetch from
-HOST = secrets["host"]
-PORT = 12345
+HOST = settings["LOGGING_HOST"]
+PORT = settings["LOGGING_PORT"] 
 TIMEOUT = 5
 INTERVAL = 5
 MAXBUF = 256
 
-#  connect to WIFI
-print("Connecting to %s"%secrets["ssid"])
-wifi.radio.connect(secrets["ssid"], secrets["password"])
-print("Connected to %s!"%secrets["ssid"])
+# connect to WiFi 
+WIFI_SSID = settings["WIFI_SSID"]
+WIFI_PASSWORD = settings["WIFI_PASSWORD"]
+print(f'Connecting to {WIFI_SSID}')
+wifi.radio.connect(WIFI_SSID, WIFI_PASSWORD)
+print(f'Connected to {WIFI_SSID}')
 
+while True:
+    pass
+
+"""
 pool = socketpool.SocketPool(wifi.radio)
 
 ipv4 = ipaddress.ip_address(pool.getaddrinfo(HOST, PORT)[0][4][0])
@@ -63,3 +73,5 @@ while True:
                                            "n", str(note), ";"])))
         #  new_val is reset
         new_val = False
+
+"""
