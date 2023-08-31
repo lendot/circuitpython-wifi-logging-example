39bfb5510a33912a409592dcf008abc185643470
Len Lanphar <lanphar@gmail.com>
2023-08-30 21:55:07 -0400

    made server more robust, added csv logging

--- server/server.py
+++ server/server.py
@@ -1,40 +1,80 @@
 from flask import Flask,request
+import csv
 
-app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "<p>Hello, World! This is an example server</p>"
+# where to store the data. We'll use a csv file
+LOG_FILE = "data.csv"
 
-"""
-This is the URL that logging data will be sent to. It supports both GET and POST
-methods, so the devices can use either of those. GET is fine for starters and is a 
-little easier to debug, but if the data being sent starts getting complex, it might
-make more sense to switch to POST
-"""
-@app.route("/log",methods=['GET','POST'])
-def log():
 
-    # get the parameters. In this example they are x, y, and z. Add/change as appropriate
-    # for your project 
-    x_str = request.args.get('x')
-    y_str = request.args.get('y')
-    z_str = request.args.get('z')
+def create_app(test_config = None, prod = True):
+    """ application factory; creates and initializes the server """
 
-    # convert paramaters from strings to their native types (in this case, float)
-    x = y = z = None
-    if x_str is not None:
-        x = float(x_str)
-    if y_str is not None:
-        y = float(y_str)
-    if z_str is not None:
-        z = float(z_str)
-         
-    print(f'({x},{y},{z})')
+    # where to store the data. We'll use a csv file. Change the name as needed
+    LOG_FILE = "data.csv"
 
-    return "<p>this is the data logging URL</p>"
+
+    app = Flask(__name__)
+
+    # put server initialization code here (log file setup, db connections, etc)
+
+    # initialize the csv file for storing data
+    print(f'Writing data to {LOG_FILE}')
+    
+    with open(LOG_FILE,'w') as logfile:
+        csv_writer = csv.writer(logfile,delimiter=',')
+        csv_writer.writerow(['x','y','z'])
+
+    app.config.update(
+            LOG_FILE = LOG_FILE 
+    )
+
+
+    @app.route("/")
+    def index():
+        return "<p>Hello, World! This is an example server</p>"
+
+    """
+    This is the URL that logging data will be sent to. It supports both GET and POST
+    methods, so the devices can use either of those. GET is fine for starters and is a 
+    little easier to debug, but if the data being sent starts getting complex, it might
+    make more sense to switch to POST
+    """
+    @app.route("/log",methods=['GET','POST'])
+    def log():
+
+        # get the parameters. In this example they are x, y, and z. Add/change as appropriate
+        # for your project 
+        x_str = request.args.get('x')
+        y_str = request.args.get('y')
+        z_str = request.args.get('z')
+
+        # convert paramaters from strings to their native types (in this case, float)
+        x = y = z = None
+        if x_str is not None:
+            x = float(x_str)
+        if y_str is not None:
+            y = float(y_str)
+        if z_str is not None:
+            z = float(z_str)
+             
+        print(f'({x},{y},{z})')
+
+        # put the data in the csv file
+        logfile = app.config['LOG_FILE']
+        with open(logfile,'a') as csv_file:
+            csv_writer = csv.writer(csv_file,delimiter=',')
+            csv_writer.writerow([x,y,z])
+
+
+
+        return "<p>this is the data logging URL</p>"
+
+    return app
 
 
 if __name__ == "__main__":
+    # initialize the server
+    app = create_app()
     # run the server so that it accepts connections from any any address
     app.run(host="0.0.0.0")
+
